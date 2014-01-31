a1acf4dbcfcc44cfc30c25a0ac9dcfca96f9295e
Luca Lusso <llusso@wellnet.it>
2014-01-31 18:28:32 +0100

    prevent array index not found warning

--- lib/Drupal/webprofiler/Form/ProfilerFormBuilder.php
+++ lib/Drupal/webprofiler/Form/ProfilerFormBuilder.php
@@ -24,8 +24,10 @@ class ProfilerFormBuilder extends FormBuilder {
    * {@inheritdoc}
    */
   public function buildForm($form_id, array &$form_state) {
-    $class = get_class($form_state['build_info']['callback_object']);
-    $this->build_forms[$form_id] = $class;
+    if (isset($form_state['build_info'])) {
+      $class = get_class($form_state['build_info']['callback_object']);
+      $this->build_forms[$form_id] = $class;
+    }
     return parent::buildForm($form_id, $form_state);
   }
 }
