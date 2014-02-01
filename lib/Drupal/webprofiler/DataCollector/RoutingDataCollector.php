f606cdb8178d0190c21124314d54c4b9bc2b8914
Daniel Wehner <daniel.wehner@erdfisch.de>
2014-02-01 14:51:56 +0100

    add basic support to show all available routes

---
+++ lib/Drupal/webprofiler/DataCollector/RoutingDataCollector.php
@@ -0,0 +1,63 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\webprofiler\DataCollector\RoutingDataCollector.
+ */
+
+namespace Drupal\webprofiler\DataCollector;
+
+use Drupal\Component\Utility\String;
+use Drupal\Core\Routing\RouteProviderInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\DataCollector\DataCollector;
+
+/**
+ * Provides a data collector which shows all available routes.
+ */
+class RoutingDataCollector extends DataCollector {
+
+  /**
+   * The route profiler.
+   *
+   * @var \Drupal\Core\Routing\RouteProviderInterface
+   */
+  protected $routeProvider;
+
+  /**
+   * Constructs a new RoutingDataCollector.
+   *
+   * @param \Drupal\Core\Routing\RouteProviderInterface $route_provider
+   *   The route provider.
+   */
+  public function __construct(RouteProviderInterface $route_provider) {
+    $this->routeProvider = $route_provider;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function collect(Request $request, Response $response, \Exception $exception = NULL) {
+    $this->data['routing'] = array();
+    foreach ($this->routeProvider->getAllRoutes() as $route_name => $route) {
+      // @TODO Find a better visual representation.
+      $this->data['routing'][String::checkPlain($route_name)] = $route->getPath();
+    }
+  }
+
+  /**
+   * Twig callback for displaying the routes.
+   */
+  public function routing() {
+    return $this->data['routing'];
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getName() {
+    return 'routing';
+  }
+
+} 
