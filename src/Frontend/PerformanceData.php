422007e394d3f997c22dab369c73967820f6b67c
Luca Lusso <llusso@wellnet.it>
2015-02-26 10:22:40 +0100

    Add a condition before returning data

--- src/Frontend/PerformanceData.php
+++ src/Frontend/PerformanceData.php
@@ -7,51 +7,71 @@ namespace Drupal\webprofiler\Frontend;
  */
 class PerformanceData {
 
-  /**
-   * @var array
-   */
-  private $data;
+	/**
+	 * @var array
+	 */
+	private $data;
 
-  /**
-   * @param array $data
-   */
-  public function __construct($data) {
-    $this->data = $data;
-  }
+	/**
+	 * @param array $data
+	 */
+	public function __construct($data) {
+		$this->data = $data;
+	}
 
-  /**
-   * @return int
-   */
-  public function getDNSTiming() {
-    return $this->data['domainLookupEnd'] - $this->data['domainLookupStart'];
-  }
+	/**
+	 * @return int
+	 */
+	public function getDNSTiming() {
+		if (isset($this->data['domainLookupEnd']) && isset($this->data['domainLookupStart'])) {
+			return $this->data['domainLookupEnd'] - $this->data['domainLookupStart'];
+		} else {
+			return 0;
+		}
+	}
 
-  /**
-   * @return int
-   */
-  public function getTCPTiming() {
-    return $this->data['connectEnd'] - $this->data['connectStart'];
-  }
+	/**
+	 * @return int
+	 */
+	public function getTCPTiming() {
+		if (isset($this->data['connectEnd']) && isset($this->data['connectStart'])) {
+			return $this->data['connectEnd'] - $this->data['connectStart'];
+		} else {
+			return 0;
+		}
+	}
 
-  /**
-   * @return int
-   */
-  public function getTtfbTiming() {
-    return $this->data['responseStart'] - $this->data['connectEnd'];
-  }
+	/**
+	 * @return int
+	 */
+	public function getTtfbTiming() {
+		if (isset($this->data['responseStart']) && isset($this->data['connectEnd'])) {
+			return $this->data['responseStart'] - $this->data['connectEnd'];
+		} else {
+			return 0;
+		}
+	}
 
-  /**
-   * @return int
-   */
-  public function getDataTiming() {
-    return $this->data['responseEnd'] - $this->data['responseStart'];
-  }
+	/**
+	 * @return int
+	 */
+	public function getDataTiming() {
+		if (isset($this->data['responseEnd']) && isset($this->data['responseStart'])) {
+			return $this->data['responseEnd'] - $this->data['responseStart'];
+		} else {
+			return 0;
+		}
+	}
 
-  /**
-   * @return int
-   */
-  public function getDomTiming() {
-    return $this->data['loadEventStart'] - $this->data['responseEnd'];
-  }
+	/**
+	 * @return int
+	 */
+	public function getDomTiming() {
+		if (isset($this->data['loadEventStart']) && isset($this->data['responseEnd'])) {
+			return $this->data['loadEventStart'] - $this->data['responseEnd'];
+		} else {
+			return 0;
+		}
+	}
 
 }
